bb0b7931a29f6897edb51f006df89c974b0da5b0
elitofsly <73740809+elitofsly@users.noreply.github.com>
2021-02-21 21:21:58 +0100

    HTML_2

--- kox-web/bejelentkezes.js
+++
@@ -1,3 +0,0 @@
-function bejelentkezes(){
-    navigacio('adatok');
-}
--- kox-web/navigacio.js
+++
@@ -1,6 +0,0 @@
-function navigacio(p){
-    location.replace(p+=".html");
-}
-function kijelentkezes(){
-    navigacio('bejelentkezes');
-}
