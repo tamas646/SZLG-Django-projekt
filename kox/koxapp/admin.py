b2291cf3747aba887296eb73ad7a094c45e67eca
tamas646 <37243375+tamas646@users.noreply.github.com>
2021-02-24 18:35:54 +0100

    Add models to admin site

--- kox/koxapp/admin.py
+++ kox/koxapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from koxapp.models import Felhasznalo, Bevitel, MozgasTipus, Mozgas, Etel
 
 # Register your models here.
+
+admin.site.register(Felhasznalo)
+admin.site.register(Bevitel)
+admin.site.register(MozgasTipus)
+admin.site.register(Mozgas)
+admin.site.register(Etel)
